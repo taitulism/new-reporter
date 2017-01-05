57f8a614631271adace5ac9cd1314f723e8dcbff
Itai Tenenbaum <taitulism@gmail.com>
2017-01-05 02:43:07 +0200

    WIP

--- test/new-task.spec.js
+++ test/new-task.spec.js
@@ -1,10 +1,63 @@
+'use strict';
+
 const expect  = require('chai').expect;
 const newTask = require('../index');
 
+const newTask = require('./task');
+
+const do_x = (obj, mainTask) => {
+    setTimeout(function() {
+        obj.a = 1;
+        mainTask.reportDone(null, obj);
+    }, 10);
+};
+
+const do_y = (obj, mainTask) => {
+    obj.files = [];
+
+    setTimeout(function() {
+        const entries = ['file1', 'file2', 'file3'];
+        const subTask = mainTask.newTask(entries.length);
+
+        entries.forEach((file) => {
+            do_z(subTask, obj, file);
+        });
+
+    }, 5);
+};
+
+const do_z = (subTask, obj, file) => {
+    setTimeout(() => {
+        obj.files.push(file);
+        subTask.reportDone(null, obj);
+    }, 0)
+};
+
+
+function fn(param, callback) {
+    const obj = {};
+
+    const mainTask = newTask(2, callback);
+
+    do_x(obj, mainTask);
+    do_y(obj, mainTask);
+}
+
+// ------------------------------------------
+console.log('wait 4 seconds...');
+fn('param', (err, obj) => {
+    console.log('--- gr8 sxs! ---');
+    console.log('err:', err);
+    console.log('return', obj);
+});
+
+
+
+
+
 describe('newTask', () => {
     it('is a function', () => {
-        const  = module();
-
         expect(newTask).to.be.a.function;
     });
 });
+
