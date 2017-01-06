3bf475ff93378b395c3c35a4f36560f18f964a04
Taitu Lizenbaum <taitulism@gmail.com>
2017-01-06 18:59:22 +0200

    init

---
+++ new-task.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = newTask;
+
+/*
+ │  "newTask" can run as a standalone function or as a Task's method.
+ │  If it's a Task's method - "this" is the parent Task, 
+ │  const parentTask = this;
+*/
+function newTask (len, callback) {
+    if (!callback && isTask(this)) {
+        return new Task(len, (...args) => {
+            // a subTask's end callback is its parentTask's .reportDone() method
+            this.reportDone(...args);
+        });
+    }
+
+    return new Task(len, callback);
+}
+
+function isTask (obj) {
+    return obj instanceof Task;
+}
+
+/* CONSTRUCTOR */
+function Task (len, callback) {
+    this.totalSubTasks = len;
+    this.callback      = callback;
+    this.done          = 0;
+}
+
+const TaskProto = Task.prototype;
+
+TaskProto.newTask = newTask;
+
+TaskProto.reportDone = function (...args) {
+    this.done++;
+
+    // check if complete (all done)
+    if (this.done === this.totalSubTasks) {
+        this.callback(...args);
+    }
+};
+
--- test/new-task.spec.js
+++ test/new-task.spec.js
@@ -1,10 +1,67 @@
+'use strict';
+
 const expect  = require('chai').expect;
-const newTask = require('../index');
+const newTask = require('../new-task');
 
-describe('newTask', () => {
+function do_something (obj, mainTask) {
+    setTimeout(function() {
+        obj.a = 1;
+        mainTask.reportDone(null, obj);
+    }, 10);
+}
+
+function do_anotherThing (obj, mainTask) {
+    obj.files = [];
+
+    setTimeout(() => {
+        const entries = ['file1', 'file2', 'file3'];
+        const subTask = mainTask.newTask(entries.length);
+
+        entries.forEach((file) => {
+            do_stuff(subTask, obj, file);
+        });
+
+    }, 5);
+}
+
+function do_stuff (subTask, obj, file) {
+    setTimeout(() => {
+        obj.files.push(file);
+        subTask.reportDone(null, obj);
+    }, 0)
+}
+
+
+function fn(param, callback) {
+    const obj = {};
+
+    const mainTask = newTask(2, callback);
+
+    do_something(obj, mainTask);
+    do_anotherThing(obj, mainTask);
+}
+
+// ------------------------------------------
+console.log('wait 4 seconds...');
+fn('param', (err, obj) => {
+    console.log('--- gr8 sxs! ---');
+    console.log('err:', err);
+    console.log('return', obj);
+});
+
+
+
+
+
+describe('newTask', (done) => {
     it('is a function', () => {
-        const  = module();
+        expect(newTask).to.be.a.function;
+    });
 
+    it('returns a Task instance', () => {
+        const x = newTask();
+        console.log(x);
         expect(newTask).to.be.a.function;
     });
 });
+
