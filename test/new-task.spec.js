e2c8323df4b88cf212a41e51701a48d0fd7abe59
Itai Tenenbaum <taitulism@gmail.com>
2017-01-08 23:04:07 +0200

    rename filename; update package.json;

--- test/new-task.spec.js
+++ test/new-task.spec.js
@@ -2,7 +2,7 @@
 
 const sinon   = require('sinon');
 const expect  = require('chai').expect;
-const newReporter = require('../new-task');
+const newReporter = require('../new-reporter');
 
 function noop () {}
 
