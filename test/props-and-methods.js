e865bb4ee6d655b426a01f0f9c161d129c4956aa
Taitu Lizenbaum <taitulism@gmail.com>
2017-03-18 11:48:46 +0200

    split tests

---
+++ test/props-and-methods.js
@@ -0,0 +1,82 @@
+const sinon   = require('sinon');
+const expect  = require('chai').expect;
+
+const newReporter = require('../');
+
+function noop () {}
+
+const rootReporter = newReporter(noop);
+const ReporterConstructor = rootReporter.constructor;
+
+describe('props', () => {
+    const reporter = newReporter(noop);
+    
+    describe('.name', () => {
+        it('is an optional argument', () => {
+            expect(reporter.name).to.equal('reporter_3');
+        });
+
+        it('is a string', () => {
+            expect(reporter.name).to.be.a.string;
+        });
+    });
+    
+    describe('.totalTasks', () => {
+        it('is a number', () => {
+            expect(reporter.totalTasks).to.be.a.number;
+        });
+
+        it('an optional argument', () => {
+            expect(rootReporter.totalTasks).to.be.a.number;
+        });
+
+        it('its default value is 2', () => {
+            expect(rootReporter.totalTasks).to.equal(2);
+        });
+    });
+
+    describe('.callback', () => {
+        it('is a function', () => {
+            expect(reporter.callback).to.be.a.function;
+        });
+
+        it('passed as an argument', () => {
+            expect(reporter.callback).to.equal(noop);
+        });
+    });
+
+    describe('.data', () => {
+        it('is an empty object', () => {
+            expect(reporter.data).to.be.an.object;
+            expect(reporter.data).to.deep.equal({});
+        });            
+    });
+
+    describe('.tmpObj', () => {
+        it('is an empty object', () => {
+            expect(reporter.tmpObj).to.be.an.object;
+            expect(reporter.tmpObj).to.deep.equal({});
+        });            
+    });
+
+    describe('.done', () => {
+        it('is a number', () => {
+            expect(reporter.done).to.be.a.number;
+        });
+        it('starts at 0', () => {
+            expect(reporter.done).to.equal(0);
+        });
+    });
+});
+
+describe('methods', () => {
+    const reporter = newReporter(2, noop);
+
+    it('.taskDone()', () => {
+        expect(reporter.taskDone).to.be.a.function;
+    });
+
+    it('.subReporter()', () => {
+        expect(reporter.subReporter).to.be.a.function;
+    });            
+});
