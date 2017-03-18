98311c17de9462d081f0ecf7fd3ce5976f90e80f
Taitu Lizenbaum <taitulism@gmail.com>
2017-03-18 23:08:31 +0200

    remove tmpData object

--- test/props-and-methods.js
+++ test/props-and-methods.js
@@ -52,13 +52,6 @@ describe('props', () => {
         });            
     });
 
-    describe('.tmpObj', () => {
-        it('is an empty object', () => {
-            expect(reporter.tmpObj).to.be.an.object;
-            expect(reporter.tmpObj).to.deep.equal({});
-        });            
-    });
-
     describe('.done', () => {
         it('is a number', () => {
             expect(reporter.done).to.be.a.number;
