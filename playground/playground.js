2cf0bbb7851e9a296ed7f54500bfb3dfeb0e2edf
Itai Tenenbaum <taitulism@gmail.com>
2017-03-19 19:54:16 +0200

    playground

--- playground/playground.js
+++ playground/playground.js
@@ -3,17 +3,12 @@ const path = require('path');
 
 const newReporter = require('../');
 
-getFolderTotalSize('lib', ({totalSize}) => {
-    console.log(totalSize);
-});
 
-// -----------------------------------------------------------------
-
-function getFolderTotalSize (folderPath, userCallback) {
+function getFolderTotalSize1 (folderPath, callback) {
+    // const x = path.resolve(folderPath)
+    // console.log(x);
     fs.readdir(folderPath, (err, files) => {
-        // always handle errors
-
-        const reporter = newReporter(files.length, userCallback);
+        const reporter = newReporter(files.length, callback);
 
         reporter.data.totalSize  = 0;
         reporter.data.folderPath = folderPath;
@@ -23,7 +18,7 @@ function getFolderTotalSize (folderPath, userCallback) {
 }
 
 function sumUpAllFilesSize (files, reporter) {
-    const folderPath = reporter.data.folderPath;
+    const {folderPath} = reporter.data;
 
     files.forEach((file) => {
         const filePath = path.join(folderPath, file);
@@ -34,9 +29,48 @@ function sumUpAllFilesSize (files, reporter) {
 
 function forEachFile (filePath, reporter) {
     fs.stat(filePath, (err, stat) => {
-        // always handle errors
+        if (err) {
+            reporter.taskFail(err);
+            return;
+        }
 
         reporter.data.totalSize += stat.size;
         reporter.taskDone();
     });
 }
+
+getFolderTotalSize1('/home/taitu/code/repos/new-reporter/test', (err, {totalSize}) => {
+    console.log(err);
+    console.log(totalSize);
+});
+
+// -----------------------------------------------------------------
+
+
+function getFolderTotalSize (folderPath, userCallback) {
+    let totalSize = 0;
+
+    fs.readdir(folderPath, (err, files) => {
+        // always handle errors
+
+        let countDown = files.length;
+
+        files.forEach((file) => {
+            const filePath = path.join(folderPath, file);
+
+            fs.stat(filePath, (err, stat) => {
+                totalSize += stat.size;
+
+                countDown--;
+
+                if (countDown === 0) {
+                    userCallback(totalSize);
+                }
+            });
+        });
+    });
+}
+
+// getFolderTotalSize('/home/taitu/code/repos/new-reporter/test', (data) => {
+//     console.log(data);
+// })
